c73f3d3260960691cbfc63e3734ab3b915398cbb
sharkstoned <stanlyraschinkin@gmail.com>
2019-08-02 22:49:59 +0300

    forward message instead of simply send; filtration config example

--- app/app.py
+++ app/app.py
@@ -23,7 +23,7 @@ source_chats = [int(source) for source in CONFIG['creds']['source_chats']]
 async def handle_message(event):
     if check_message(event.message, FILTER_QUERY):
         for reciever in CONFIG['creds']['target_chats']:
-            await client.send_message(int(reciever), event.message)
+            await client.forward_messages(int(reciever), event.message, event.from_id)
 
 client.run_until_disconnected()
 
